d62b4292ab3b732be3aa12cec4cbb6acda8f723c
haberben <ozethabertr@gmail.com>
2026-02-12 19:08:27 +0300

    Simplify tailwind.config.js by removing fontFamily
    Removed fontFamily configuration from Tailwind CSS setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'brand-dark': '#1A3636',   // Koyu, lüks yeşil
-        'brand-gold': '#D6BD98',   // Altın vizon (vurgu rengi)
-        'brand-light': '#F8F9F8',  // Saf temiz arka plan
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        serif: ['Playfair Display', 'serif'], // Başlıklar için lüks font
+        'brand-dark': '#1A3636',
+        'brand-gold': '#D6BD98',
+        'brand-light': '#F8F9F8',
       },
     },
   },
