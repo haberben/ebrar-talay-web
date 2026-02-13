c54fb31df216b6b84c112d3b51979bbeebc2b62f
haberben <ozethabertr@gmail.com>
2026-02-13 09:33:49 +0300

    Format tailwind.config.js for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         'brand-dark': '#1A3636',
         'brand-gold': '#D6BD98',
-        'brand-light': '#F8F9F8',
-      },
-    },
+        'brand-light': '#F8F9F8'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
